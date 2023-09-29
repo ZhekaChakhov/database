25c69412cc596dcf3983cdfc0e1446497e5c9ae0
Zheka Chakhov <zhekachakhov@mail.ru>
2023-09-29 20:02:56 +0300

    change 5 to 8

--- validations/auth.js
+++ validations/auth.js
@@ -3,8 +3,8 @@ import { body } from "express-validator";
 // проверка данных на валидность
 export const registerValidation = [
 	body("email", "Неверный формат почты").isEmail(),
-	body("password", "Пароль должен быть минимум 5 символов").isLength({
-		min: 5,
+	body("password", "Пароль должен быть минимум 8 символов").isLength({
+		min: 8,
 	}),
 	body("fullName", "Укажите имя").isLength({ min: 3 }),
 	body("avatarUrl", "Неверная ссылка на аватарку").optional().isURL(),
