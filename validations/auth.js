e8de8adf5891b83115679928b704c558d0ec721c
Zheka Chakhov <zhekachakhov@mail.ru>
2023-10-02 13:43:43 +0300

    edit passwordValidation

--- validations/auth.js
+++ validations/auth.js
@@ -3,9 +3,17 @@ import { body } from "express-validator";
 // проверка данных на валидность
 export const registerValidation = [
 	body("email", "Неверный формат почты").isEmail(),
-	body("password", "Пароль должен быть минимум 8 символов").isLength({
-		min: 8,
-	}),
+	body("password", "Неверный формат пароля")
+		.isLength({
+			min: 8,
+		})
+		.custom((value) => {
+			return (
+				!(value === value.toUpperCase()) &&
+				!(value === value.toLowerCase()) &&
+				/[0-9]/.test(value)
+			);
+		}),
 	body("fullName", "Укажите имя").isLength({ min: 3 }),
 	body("avatarUrl", "Неверная ссылка на аватарку").optional().isURL(),
 ];
